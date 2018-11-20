778ceaf68e4720c9bdf517f17e576a7870cee15a
DBdiego <diego.de.buysscher@gmail.com>
2018-11-21 00:24:32 +0100

    added valuable input sources such as the aircraft type to capacity and the distance between terminals and bays

---
+++ MAIN.py
@@ -0,0 +1,33 @@
+from scipy.optimize import linprog
+from datetime import datetime
+import pandas as pd
+import numpy as np
+from math import *
+
+#Importing other file
+import data_importer as DI
+
+simulation_cases = DI.simulation_cases
+a2c = DI.a2c
+bay_distances = DI.bay_distances
+
+
+
+
+
+
+c = [-1, 4]
+A = [[-3, 1], [1, 2]]
+b = [6, 4]
+
+x0_bounds = (None, None)
+x1_bounds = (-3, None)
+bounds_ = []
+
+for i in range(len(c)):
+    bounds_.append((0,1))
+
+    
+res = linprog(c, A_ub=A, b_ub=b, bounds=bounds_, options={"disp": True})
+print(res)
+
--- data_importer.py
+++ data_importer.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import numpy as np
 from math import *
 import os
@@ -10,6 +11,8 @@ from datetime import datetime
 base_directory = './csv_data_appendices'
 
 files = [x for x in os.listdir(base_directory) if 'Simulation Case ' in x]
+
+
 files.sort()
 
 simulation_cases = {}
@@ -18,7 +21,7 @@ print ('Importing info: ...')
 for file in files:
 
     #Importing Data
-    imported_data = pd.read_csv(base_directory+'/'+file, sep=',')
+    imported_data = pd.read_csv(open(base_directory+'/'+file), sep=',')
     simulation_cases.update({file[-6:-4]:imported_data})
     print('  '+file+': DONE')
 
@@ -26,8 +29,10 @@ for file in files:
     #Showing Aircraft Ground Time
     plot_requirements = 0
     if plot_requirements:
+    
         arrival_departure = imported_data[['ATA', 'ATD']]
         fig, ax = plt.subplots()
+        
         for i in range(len(arrival_departure['ATA'])):
             
             ata = pd.to_datetime(arrival_departure['ATA'][i])
@@ -42,8 +47,20 @@ for file in files:
                      datetime.now().replace(hour=23, minute=59, second=59, microsecond=0)])
         plt.show()
 
+
+a2c = pd.read_csv(open(base_directory+'/Aircraft_type2capacity.csv'), sep=',')
+bay_distances = pd.read_csv(open(base_directory+'/Bay Distances.csv'), sep=',') 
+
+
+'''
+result_files = ['Bay Assignments Results 02-06-2015.csv',
+                'Bay Assignments Results 05-07-2015.csv']
+
+results = []
+for result_file in result_files:
+    results.append(pd.read_csv(open(base_directory+'/'+result_file), sep=','))
+'''
+
 print ('Importing info: DONE')
     
-
-
 
