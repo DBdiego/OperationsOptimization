f51c2783afd641a626b84040d7ad04d44e223d01
DBdiego <diego.de.buysscher@gmail.com>
2018-11-22 11:40:37 +0100

    some last changes due to inconsistent input data

--- Program/MAIN.py
+++ Program/MAIN.py
@@ -18,41 +18,53 @@ aircraft_type2group     = DI.aircraft_type2group
 
 
 
-
 simulation_case = simulation_cases['01']
 
+terminal = 'A'
+
+z1_coefficients = []
+z2_coefficients = []
+
 # Determining coefficients of decision variables
-for i in range(len(simulation_case)):
-    flight_number = simulation_case['Fl No.'][i]
+
+flight_numbers = simulation_case['Fl No.'].unique()
+for i in range(len(flight_numbers)):
+    flight_number = flight_numbers[i]
     
     if flight_number in flight_no2aircraft_type:
 
         #Find number of passengers in an aircraft
         aircraft_type     = flight_no2aircraft_type[flight_number]
-        aircraft_capacity = aircraft_type2capacity[aircraft_type]['Capacity']
+        aircraft_capacity = int(aircraft_type2capacity[aircraft_type]['Capacity'])
         aircraft_group    = aircraft_type2group[aircraft_type]
+
+        #Possible bays for aircraft model
         bay_compliance    = group2bay_compliance[['Bay', aircraft_group]] 
-        print (flight_number, aircraft_type, aircraft_group, aircraft_capacity)
+        possible_bays = list(bay_compliance.loc[bay_compliance[aircraft_group].isin([1])]['Bay'].unique())
+        #preference
+        print (flight_number, aircraft_type, aircraft_group, aircraft_capacity, preference)
         
-        for j in range(len(bay_distances)):
+        for j in range(len(possible_bays)):
+            bay = possible_bays[j]
+            terminal_distances = bay_distances[['Bay', terminal]]
+            distance = int(terminal_distances.loc[terminal_distances['Bay'].isin([bay])].iloc[0, 1])
+            z1_coefficients.append(aircraft_capacity * distance)
+
             
-            
-            
-            
-            pass
+
+z1_coefficients = np.array(z1_coefficients)
+z2_coefficients = np.array(z2_coefficients)
 
 
 
 
 
-c = [-1, 4]
+
+c = [-1, 4] #coefficients
 A = [[-3, 1], [1, 2]]
 b = [6, 4]
 
-x0_bounds = (None, None)
-x1_bounds = (-3, None)
 bounds_ = []
-
 for i in range(len(c)):
     bounds_.append((0,1))
 
