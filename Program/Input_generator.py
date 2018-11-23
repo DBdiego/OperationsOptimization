1980cc4dd0b305c7392fd03016b8c128a61331ab
Gervase Prescod <gervase.prescod@gmail.com>
2018-11-23 17:37:21 +0100

    added lines to Input_generator.py

--- Program/Input_generator.py
+++ Program/Input_generator.py
@@ -11,13 +11,16 @@ from math import *
 # Imported data from data_importer script
 import data_importer as DI
 
+# Directory definition
+base_directory = '../csv_data_appendices/input_distributions'
 
-#total_num_flights = len(DI.imported_data['Fl No. Arrival'].unique())
+# Reading AC type distribution
+AC_type_distr = pd.read_csv(open(base_directory+'/AC_type_distribution.csv'), sep=',')
 
+# Reading AC ATA distribution
+time_slot_size = '10.csv'     
+AC_ATA_distr = pd.read_csv(open(base_directory+'/arrival_sampling_'+time_slot_size), sep=',')
+AC_ATA_distr['Time'] = pd.to_datetime(AC_ATA_distr['Time'])
 
-'''
-result = np.random.choice([AC Types], p=[Probs])
-'''
-
-
-
+AC_type = np.random.choice(list(AC_type_distr['AC Type']), p=list(AC_type_distr['Probs']))
+AC_ATA = np.random.choice(list(AC_ATA_distr['Time']), p=list(AC_ATA_distr['Probs']))
