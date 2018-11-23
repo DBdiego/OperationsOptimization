2fe0651fa032f54b30d7aed74828d13ff9842ef2
DBdiego <diego.de.buysscher@gmail.com>
2018-11-23 16:47:50 +0100

    Input_generator.py was added

---
+++ Program/Input_generator.py
@@ -0,0 +1,23 @@
+import matplotlib.dates  as mdates
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import csv
+import os
+
+import datetime
+from math import *
+
+# Imported data from data_importer script
+import data_importer as DI
+
+
+#total_num_flights = len(DI.imported_data['Fl No. Arrival'].unique())
+
+
+'''
+result = np.random.choice([AC Types], p=[Probs])
+'''
+
+
+
