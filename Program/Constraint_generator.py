f7746b88f9a0c00c815cd594c1c64beef879b22d
DBdiego <diego.de.buysscher@gmail.com>
2018-11-27 20:03:46 +0100

    fuel constraint  function moved
    moved the function to the global contraint_generator.py file

--- Program/Constraint_generator.py
+++ Program/Constraint_generator.py
@@ -17,10 +17,10 @@ group2bay_compliance = DI.group2bay_compliance
 all_bays = np.array(group2bay_compliance['Bay'])
 
 
-# Bay Compliance Constraint
+# BAY COMPLIANCE Constraint
 def add_bay_compliance(input_data, Bay_Assignement, flight_vars):
+    
     print ('  ---> Adding Bay Compliance Constraints ...')
-    start_time = time.time()
     
     constraint_collection = {}
     
@@ -49,8 +49,9 @@ def add_bay_compliance(input_data, Bay_Assignement, flight_vars):
 
 
 
-# Time Constraint
+# TIME Constraint
 def add_time_constraint(input_data, Bay_Assignement, flight_vars):
+    
     print ('  ---> Adding Time Constraints ...')
 
     #Creation of matrix
@@ -95,3 +96,67 @@ def add_time_constraint(input_data, Bay_Assignement, flight_vars):
 
     return Bay_Assignement, time_conflict_matrix
 
+
+
+
+
+# FUEL Constraint
+#Non serviceable bays : J7, J8, J9 and the STPV bays (STPV1/2)
+def add_fuelling_constraint(input_data, Bay_Assignement, flight_vars):
+    print('  ---> Adding Fuelling Constraints ...')
+
+    constraint_collection = {}
+
+    # Going Through all flights
+    for i, flight in enumerate(input_data):
+
+        # Finding the flight type
+        long_stay = flight['long stay']
+        move_type = flight['move type']
+        domestic  = flight['connection']=='DOM'
+
+
+        # Defining serviceable bays
+        bay_list = list(group2bay_compliance['Bay'])
+        bay_list.remove('J7')
+        bay_list.remove('J8')
+        bay_list.remove('J9')
+            # STPV not removed because they don't even exist in the list of all bays
+
+        fuelling_bays = bay_list
+
+        # Defining coefficient of constraint
+        constraint = {}
+        constraint_vars = []
+
+        # Flight is move_type=='Full' or non-domestic during departure phase
+        if move_type== 'Full' or (domestic == 0 and move_type == 'Dep'): 
+
+            for j, bay in enumerate(fuelling_bays):
+                constraint_variable = str(i) + '_' + bay
+
+                constraint.update({constraint_variable: 1})
+                constraint_vars.append(constraint_variable)
+
+            constraint_collection.update({'F' + str(i): {'constraint': constraint,
+                                                          'variables': constraint_vars}})
+            Bay_Assignement += pulp.lpSum([constraint[i] * flight_vars[i] for i in constraint_vars]) == 1, 'F' + str(i)
+
+        # Flight is in long stay (Just domestic during parking or departure phase)
+        #   resulting in: (long_stay == 1 and domestic==1 and move_type=='Park') or ... ejected because we use i-1 in constraint
+        if (long_stay == 1 and domestic==1 and move_type=='Dep'): 
+
+            for j, bay in enumerate(fuelling_bays):
+                constraint_variables = [str(i-1) + '_' + bay , str(i) + '_' + bay]  #Take into account flight i (departure phase) and i-1 (parking phase)
+
+                constraint.update({constraint_variables[0]: 1, constraint_variables[1]: 1})
+
+                constraint_vars.append(constraint_variables[0])
+                constraint_vars.append(constraint_variables[1])
+
+            constraint_collection.update({'F' + str(i): {'constraint': constraint,
+                                                          'variables': constraint_vars}})
+            Bay_Assignement += pulp.lpSum([constraint[i] * flight_vars[i] for i in constraint_vars]) == 1, 'F' + str(i)
+
+
+    return Bay_Assignement, constraint_collection
